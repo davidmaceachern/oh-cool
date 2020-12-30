09edf1bea588345bc22a6623517d606fef71108f
David MacEachern <david.maceachern.tech@gmail.com>
2020-12-30 15:32:26 +0100

    Add call to the github graph.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use structopt::StructOpt;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Star::from_args();
-    star::run( 
+    star::run(
         env::var("GITHUB_API_KEY")
             .map_err(|_| anyhow::anyhow!(
                 "Please export a GITHUB_API_KEY env variable.\n  ▶ You can generate one by visiting https://github.com/settings/tokens/new"
--- src/star.rs
+++ src/star.rs
@@ -1,7 +1,8 @@
 use anyhow::{anyhow, bail};
-use structopt::StructOpt;
-
 use hubcaps::{Credentials, Github};
+use reqwest::{header, Client};
+use serde_json::json;
+use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 pub struct Star {
     /// Run without actually updating account
@@ -9,6 +10,7 @@ pub struct Star {
     dryrun: bool,
 }
 static USER_AGENT: &str = "oh-cool (https://github.com/davidmaceachern/oh-cool)";
+
 static GRAPHQL_QUERY_STARRED: &str = "
 query {
   viewer {
@@ -29,25 +31,45 @@ query {
       }
     }
   }
+}
 ";
 
-pub async fn run(
-    api_key: String,
-    args: Star,
-) -> anyhow::Result<()> {
-    let github = Github::new(
-        USER_AGENT,
-    Credentials::Token(api_key),
-  )?;
+pub async fn graphql(api_key: String) -> anyhow::Result<()> {
+    let endpoint = "https://api.github.com/graphql";
+    let client = Client::new();
+    let resp = client
+        .post(endpoint)
+        .header(header::AUTHORIZATION, format!("token {}", api_key))
+        .header(header::USER_AGENT, USER_AGENT)
+        .json(&json!({
+            "query": GRAPHQL_QUERY_STARRED,
+        }))
+        .send()
+        .await?;
+    let body = resp.text().await?;
+    println!("{}", body);
+    Ok(())
+}
+
+pub fn diff() {
+    unimplemented!()
+}
+
+pub async fn run(api_key: String, args: Star) -> anyhow::Result<()> {
+    let github = Github::new(USER_AGENT, Credentials::Token(api_key.clone()))?;
     let Star { dryrun } = args;
+    graphql(api_key.clone()).await?;
     // Does not consume rate limit
     let status = github.rate_limit().get().await?;
     println!("{:#?}", status);
-    if !dryrun {
-            let repositories = github.user_repos("davidmaceachern").list(&Default::default()).await?;
-            // println!("You would have starred the following repositories");
-            println!("{:?}", repositories);
-        }
+    if dryrun {
+        let repositories = github
+            .user_repos("davidmaceachern")
+            .list(&Default::default())
+            .await?;
+        // println!("You would have starred the following repositories");
+        println!("{:?}", repositories);
+    }
     Ok(())
 }
 
@@ -57,7 +79,7 @@ pub async fn run(
 
 //     #[test]
 //     fn test_compares_lists() {
-//         let result = run();
+//         let result = diff();
 //         assert_eq!(result, expected);
 //     }
 // }
